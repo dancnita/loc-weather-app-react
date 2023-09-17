beabbc7e4125949b170c946a20494c1397617a0c
dancnita <dan.c.nita@gmail.com>
2023-09-17 14:16:10 +0300

    mixed content meta

--- locWeatherApp/vite.config.js
+++ locWeatherApp/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/loc-weather-app-react/',
   plugins: [react()],
-})
+});
